75fbc6a16fb72deeb514518b6dcfd5dad9da3acc
Paul Luis Roehl <roehl@insature.net>
2026-09-15 20:51:19 +0100

    add @astrojs/mdx and photoswipe
    also, npm audit fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import { unified } from "@astrojs/markdown-remark";
 import remarkMath from "remark-math";
 import rehypeTemml from "./src/lib/rehype-temml.js";
 
 export default defineConfig({
   site: "https://insature.net",
+  integrations: [mdx()],
   markdown: {
     processor: unified({
       remarkPlugins: [remarkMath],
